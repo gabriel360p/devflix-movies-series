1f9aa111057a6145dc777e8c932d0451d6202c0c
gabriel360p <gabrielmark210@gmail.com>
2026-07-27 20:17:56 -0300

    fix: correção de imports

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 
 import Home from "../containers/Home";
-import Movies from "../containers/movies";
-import Series from "../containers/series";
+import Movies from "../containers/Movies";
+import Series from "../containers/Series";
 import Detail from "../containers/Detail";
 import DefaultLayout from "../layout/DefaultLayout";
 
